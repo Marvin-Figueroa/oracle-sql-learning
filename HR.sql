5df7bb9ac6ce560b8e8315615d6b708757557698
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2023-09-13 15:44:36 -0600

    Add practices with SELECT

--- HR.sql
+++ HR.sql
@@ -1,3 +1,27 @@
 -- Curso - Aprende Oracle SQL Desde Cero - Udemy - Ejercicios Resueltos
 
+----------------------------------------------------------------------------------------------------------------------------
+-- PRACTICAS CON SELECT
+----------------------------------------------------------------------------------------------------------------------------
+
+-- Visualizar el nombre y el número de teléfono de los empleados
+SELECT CONCAT(CONCAT(first_name, ' '), last_name) AS name, phone_number AS phone FROM employees;
+
+-- Visualizar el nombre y el tipo de trabajo de los empleados (FIRST_NAME, JOB_ID). 
+-- Debe aparecer en la cabecera NOMBRE Y Tipo de Trabajo.
+SELECT CONCAT(CONCAT(e.first_name, ' '), e.last_name) AS name, j.job_title AS "TIPO TRABAJO" 
+FROM employees e JOIN jobs j USING(job_id);
+
+-- Selecciona todas las columnas de la tabla REGIONS
+SELECT * FROM regions;
+
+-- Indicar los nombres de los países de la tabla COUNTRIES
+SELECT country_name FROM countries;
+
+-- Seleccionar las columnas STREET_ADDRESS, CITY, STATE_PROVINCE de la table LOCATIONS. 
+-- Debemos poner las columnas como dirección, Ciudad y Estado
+SELECT street_address AS dirección, city AS ciudad, state_province AS estado FROM locations;
+
+----------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------
 
