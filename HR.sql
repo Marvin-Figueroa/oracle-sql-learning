e7c091faf4bacbd39dbbc13b7939cd59bc76b976
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2023-09-14 08:53:53 -0600

    Add practices with WHERE

--- HR.sql
+++ HR.sql
@@ -59,3 +59,30 @@ SELECT DISTINCT city FROM locations;
 
 -- Visualizar los distintos tipos de JOB_ID por departamento de la tabla Employees
 SELECT DISTINCT department_id, job_id FROM employees;
+
+----------------------------------------------------------------------------------------------------------------------------
+-- PRACTICAS CON WHERE
+----------------------------------------------------------------------------------------------------------------------------
+
+-- Averigua los empleados que trabajen en el departamento 100
+SELECT * FROM employees WHERE department_id = 100;
+
+-- Usando la tabla LOCATIONS, averigua el nombre de la Ciudad (city) y la dirección 
+-- (Street_address) de los departamentos situados en Estados Unidos (COUNTRY_ID=US)
+SELECT city, street_address FROM locations WHERE country_id = 'US';
+
+-- Visualiza los países que están en la región 3. (REGION_ID de la tabla COUNTRIES)
+SELECT * FROM countries WHERE region_id = 3;
+
+-- Averiguar el nombre y salario de los empleados que NO tengan como jefe al
+-- MANAGER 114 (columna MANAGER_ID)
+SELECT first_name, salary FROM employees WHERE manager_id != 114;
+
+-- Visualizar los empleados que empezaron a trabajar a partir del año 2006
+SELECT * FROM employees WHERE hire_date >= '01-01-06';
+
+-- Seleccionar los empleados que tenga como tipo de trabajo ‘ST_CLERK’
+SELECT * FROM employees WHERE job_id = 'ST_CLERK';
+
+-- Indicar los datos de los empleados que tengan como apellidos “Smith” (LAST_NAME)
+SELECT * FROM employees WHERE last_name = 'Smith';
