1c13e7166c54e543e6e857cdb7f87319af2d23f6
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2023-09-15 15:54:56 -0600

    Add practices with TO_CHAR Function

--- HR.sql
+++ HR.sql
@@ -249,3 +249,35 @@ SELECT first_name, hire_date, LAST_DAY(hire_date) FROM employees;
 -- Utilizando la función ROUND, indicar los empleados que entraron en los 
 -- últimos 15 días de cada mes.
 SELECT first_name, hire_date FROM employees WHERE ROUND(LAST_DAY(hire_date) - hire_date) <= 15;
+
+----------------------------------------------------------------------------------------------------------------------------
+-- PRACTICAS CON CONVERSION TO_CHAR
+----------------------------------------------------------------------------------------------------------------------------
+
+-- Indicar los empleados que entraron en Mayo en la empresa. Debemos
+-- buscar por la abreviatura del mes
+SELECT first_name, hire_date, TO_CHAR(hire_date, 'MON') AS "MONTH HIRE" 
+FROM employees WHERE TO_CHAR(hire_date, 'MON') = 'MAY';
+
+-- Indicar los empleados que entraron en el año 2007 usando la función to_char
+SELECT first_name, hire_date FROM employees WHERE TO_CHAR(hire_date, 'YY') = 07;
+SELECT first_name, hire_date FROM employees WHERE TO_CHAR(hire_date, 'YYYY') = 2007;
+
+-- ¿Qué día de la semana (en letra) era el día que naciste?
+SELECT TO_CHAR(TO_DATE('10-10-2010'), 'DAY') AS "DAY OF BIRTH" FROM DUAL;
+
+/*
+Averiguar los empleados que entraron en el mes de Junio. Debemos preguntar por el 
+mes en letra. Nota: La función TO_CHAR puede devolver espacios a la derecha)
+*/
+SELECT first_name, hire_date, '[' || TO_CHAR(hire_date, 'MONTH') || ']' AS "RIGHT SPACES" FROM employees;
+
+SELECT first_name, hire_date, TO_CHAR(hire_date, 'MONTH') AS "HIRE MONTH" FROM employees 
+WHERE RTRIM(TO_CHAR(hire_date, 'MONTH')) = 'JUNIO';
+
+/*
+Visualizar el salario de los empleados con dos decimales y en dólares y también en la 
+moneda local (el ejemplo es con euros, suponiendo que el cambio esté en 0,79$)
+*/
+SELECT first_name, salary, TO_CHAR(salary, '$99,999.99') AS "DOLARES", 
+TO_CHAR(salary * 8.75, 'L999,999.99') AS "COLONES(Deprecated)" FROM employees; 
